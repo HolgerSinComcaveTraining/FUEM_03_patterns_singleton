ebb7ee81db8173152421b1607a7a09f56925d36c
Peter <schmidt.multimedia@gmail.com>
2021-10-28 11:19:35 +0200

    Lösung mit Singleton

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,11 +4,33 @@ public class App {
     
     public static void main(String[] args) {
 
-        Singleton obj1 = new Singleton();
+        Singleton obj1 = Singleton.getInstance();
         Helper.ausgabe(obj1);
-
-        Singleton obj2 = new Singleton();
+        obj1.actOnEvent();
+        obj1.actOnEvent();
+        // Events werden kumuliert ... 0 + 2
+        Helper.ausgabe(obj1.getCounter());
+        
+        Singleton obj2 = Singleton.getInstance();
         Helper.ausgabe(obj2);
+        obj2.actOnEvent();
+        obj2.actOnEvent();
+        obj2.actOnEvent();
+        // Events werden kumuliert ... 2 + 3
+        Helper.ausgabe(obj2.getCounter());
+
+        Singleton obj3 = Singleton.getInstance();
+        Helper.ausgabe(obj3);
+        obj3.actOnEvent();
+        obj3.actOnEvent();
+        obj3.actOnEvent();
+        obj3.actOnEvent();
+        obj3.actOnEvent();
+        // Events werden kumuliert ... 5 + 5
+        Helper.ausgabe(obj3.getCounter());
+
+
+
 
     }
 
