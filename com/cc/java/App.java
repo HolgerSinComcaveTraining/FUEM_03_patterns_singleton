77305d761227b56d96de5915e0ceecabba8a5de1
Peter <schmidt.multimedia@gmail.com>
2021-10-28 10:27:51 +0200

    Basic Instanziierung

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,7 +4,12 @@ public class App {
     
     public static void main(String[] args) {
 
-    
+        Singleton obj1 = new Singleton();
+        Helper.ausgabe(obj1);
+
+        Singleton obj2 = new Singleton();
+        Helper.ausgabe(obj2);
+
     }
 
 }
